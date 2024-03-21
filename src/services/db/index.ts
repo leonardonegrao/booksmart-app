a56e6e470ab8c8ee5c65f2e853cf1079969a754d
Leonardo Negrao <leonardolnegrao@gmail.com>
2024-03-21 15:40:03 -0300

    feat: improve db actions reusability

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,8 +1,9 @@
 import * as SQLite from "expo-sqlite";
+
+import * as dbActions from "./db-actions";
+
 import type { SQLiteDatabase } from "expo-sqlite";
 import type { CreateBookAPIResponse } from "../api/create-book/types";
-import { Book } from "@/src/@types/book";
-import Highlight from "@/src/@types/highlight";
 
 export interface InsertBookInput extends CreateBookAPIResponse {
   bookLocalUri: string;
@@ -16,81 +17,51 @@ const openDatabase = () => {
   return db;
 };
 
-const dropBookTable = (db: SQLiteDatabase) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      "DROP TABLE IF EXISTS books;",
-      [],
-      () => transactionResult = "success" as const,
-      () => {
-        transactionResult = "error" as const;
-        return false;
-      },
-    );
-  });
-
-  return transactionResult;
-};
-
-const createBookTable = (db: SQLiteDatabase) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      `
-        CREATE TABLE IF NOT EXISTS books
-        (
-          id TEXT,
-          filename TEXT,
-          userId TEXT,
-          bookBucketKey TEXT,
-          bookLocalUri TEXT,
-          epubLocalUri TEXT,
-          coverBucketKey TEXT,
-          coverLocalUri TEXT,
-          title TEXT,
-          author TEXT,
-          percentageRead INTEGER,
-          lastLocation TEXT,
-          language TEXT
-        );`,
-    [],
-    () => transactionResult = "success" as const,
-    () => {
-      transactionResult = "error" as const;
-      return false;
-    },
-    );
-  });
-
-  return transactionResult;
-};
-
-const insertBook = (db: SQLiteDatabase, bookData: InsertBookInput) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      `
-        INSERT INTO books
-        (
-          id,
-          filename,
-          userId,
-          bookBucketKey,
-          bookLocalUri,
-          epubLocalUri,
-          coverBucketKey,
-          coverLocalUri,
-          title,
-          author,
-          percentageRead,
-          lastLocation,
-          language
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
-      [
+const books = {
+  createBooksTable: (db: SQLiteDatabase) => {
+    return dbActions.createTable({
+      db,
+      tableName: "books",
+      fields: [
+        { key: "id", type: "TEXT" },
+        { key: "filename", type: "TEXT" },
+        { key: "userId", type: "TEXT" },
+        { key: "bookBucketKey", type: "TEXT" },
+        { key: "bookLocalUri", type: "TEXT" },
+        { key: "epubLocalUri", type: "TEXT" },
+        { key: "coverBucketKey", type: "TEXT" },
+        { key: "coverLocalUri", type: "TEXT" },
+        { key: "title", type: "TEXT" },
+        { key: "author", type: "TEXT" },
+        { key: "percentageRead", type: "INTEGER" },
+        { key: "lastLocation", type: "TEXT" },
+        { key: "language", type: "TEXT" },
+      ],
+    });
+  },
+  dropBooksTable: (db: SQLiteDatabase) => {
+    return dbActions.dropTable({ db, tableName: "books" });
+  },
+  insertBook: (db: SQLiteDatabase, bookData: InsertBookInput) => {
+    return dbActions.insertRow({
+      db,
+      tableName: "books",
+      fields: [
+        "id",
+        "filename",
+        "userId",
+        "bookBucketKey",
+        "bookLocalUri",
+        "epubLocalUri",
+        "coverBucketKey",
+        "coverLocalUri",
+        "title",
+        "author",
+        "percentageRead",
+        "lastLocation",
+        "language",
+      ],
+      values: [
         bookData.id,
         bookData.filename,
         bookData.userId,
@@ -105,189 +76,102 @@ const insertBook = (db: SQLiteDatabase, bookData: InsertBookInput) => {
         bookData.lastLocation,
         bookData.language,
       ],
-      () => transactionResult = "success" as const,
-      () => {
-        transactionResult = "error" as const;
-        return false;
-      },
-    );
-  });
-
-  return transactionResult;
-};
-
-const updateBookProgress = (db: SQLiteDatabase, bookId: string, percentageRead: number, location: string) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      "UPDATE books SET percentageRead = ?, lastLocation = ? WHERE id = ?;",
-      [percentageRead, location, bookId],
-      () => transactionResult = "success" as const,
-      () => {
-        transactionResult = "error" as const;
-        return false;
-      },
-    );
-  });
-
-  return transactionResult;
-};
-
-const getBooks = (db: SQLiteDatabase, userId: string) => {
-  return new Promise<Book[]>((resolve) => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "SELECT * FROM books WHERE userId = ?;",
-        [userId],
-        (_, { rows }) => {
-          const books: Book[] = [];
-          for (let i = 0; i < rows.length; i++) {
-            books.push(rows.item(i));
-          }
-          resolve(books);
-        },
-        () => {
-          return false;
-        },
-      );
     });
-  });
-};
-
-const getBook = (bookId: string) => {
-  const db = openDatabase();
-
-  return new Promise<Book>((resolve) => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "SELECT * FROM books WHERE id = ?;",
-        [bookId],
-        (_, { rows }) => {
-          resolve(rows.item(0));
-        },
-        () => {
-          return false;
-        },
-      );
+  },
+  updateBook: (db: SQLiteDatabase, bookData: InsertBookInput) => {
+    return dbActions.insertRow({
+      db,
+      tableName: "books",
+      fields: [
+        "id",
+        "filename",
+        "userId",
+        "bookBucketKey",
+        "bookLocalUri",
+        "epubLocalUri",
+        "coverBucketKey",
+        "coverLocalUri",
+        "title",
+        "author",
+        "percentageRead",
+        "lastLocation",
+        "language",
+      ],
+      values: [
+        bookData.id,
+        bookData.filename,
+        bookData.userId,
+        bookData.bookBucketKey,
+        bookData.bookLocalUri,
+        bookData.epubLocalUri,
+        bookData.coverBucketKey,
+        bookData.coverLocalUri,
+        bookData.title,
+        bookData.author,
+        bookData.percentageRead,
+        bookData.lastLocation,
+        bookData.language,
+      ],
     });
-  });
-};
-
-const createHighlightsTable = (db: SQLiteDatabase) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      `
-        CREATE TABLE IF NOT EXISTS highlights
-        (
-          id TEXT,
-          bookId TEXT,
-          location TEXT,
-          color TEXT,
-          content TEXT
-        );`,
-    [],
-    () => transactionResult = "success" as const,
-    () => {
-      transactionResult = "error" as const;
-      return false;
-    },
-    );
-  });
-
-  return transactionResult;
-};
-
-const dropHighlightsTable = (db: SQLiteDatabase) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      "DROP TABLE IF EXISTS highlights;",
-      [],
-      () => transactionResult = "success" as const,
-      () => {
-        transactionResult = "error" as const;
-        return false;
-      },
-    );
-  });
-
-  return transactionResult;
-};
-
-const insertHighlight = (db: SQLiteDatabase, bookId: string, location: string, color: string, content: string) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      "INSERT INTO highlights (id, bookId, location, color, content) VALUES (?, ?, ?, ?, ?);",
-      [bookId + location, bookId, location, color, content],
-      () => transactionResult = "success" as const,
-      () => {
-        transactionResult = "error" as const;
-        return false;
-      },
-    );
-  });
-
-  return transactionResult;
-};
-
-const deleteHighlight = (db: SQLiteDatabase, bookId: string, location: string) => {
-  let transactionResult: null | "success" | "error" = null;
-
-  db.transaction((tx) => {
-    tx.executeSql(
-      "DELETE FROM highlights WHERE bookId = ? AND location = ?;",
-      [bookId, location],
-      () => transactionResult = "success" as const,
-      () => {
-        transactionResult = "error" as const;
-        return false;
-      },
-    );
-  });
-
-  return transactionResult;
-};
-
-const getHighlights = (bookId: string) => {
-  const db = openDatabase();
-
-  return new Promise<Highlight[]>((resolve) => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "SELECT * FROM highlights WHERE bookId = ?;",
-        [bookId],
-        (_, { rows }) => {
-          const highlights: Highlight[] = [];
-          for (let i = 0; i < rows.length; i++) {
-            highlights.push(rows.item(i));
-          }
-          resolve(highlights);
-        },
-        () => {
-          return false;
-        },
-      );
+  },
+  getBooks: (db: SQLiteDatabase) => {
+    return dbActions.getAll({ db, tableName: "books" });
+  },
+  getBook: (db: SQLiteDatabase, bookId: string) => {
+    return dbActions.getOne({
+      db,
+      tableName: "books",
+      fieldFilter: "id",
+      valueFilter: bookId,
     });
-  });
+  },
+};
+
+const highlights = {
+  createHighlightsTable: (db: SQLiteDatabase) => {
+    return dbActions.createTable({
+      db,
+      tableName: "highlights",
+      fields: [
+        { key: "id", type: "TEXT" },
+        { key: "bookId", type: "TEXT" },
+        { key: "location", type: "TEXT" },
+        { key: "color", type: "TEXT" },
+        { key: "content", type: "TEXT" },
+      ],
+    });
+  },
+  dropHighlightsTable: (db: SQLiteDatabase) => {
+    return dbActions.dropTable({ db, tableName: "highlights" });
+  },
+  insertHighlight: (db: SQLiteDatabase, bookId: string, location: string, color: string, content: string) => {
+    return dbActions.insertRow({
+      db,
+      tableName: "highlights",
+      fields: ["id", "bookId", "location", "color", "content"],
+      values: [`${bookId}-${location}`, bookId, location, color, content],
+    });
+  },
+  deleteHighlight: (db: SQLiteDatabase, highlightId: string) => {
+    return dbActions.deleteRow({
+      db,
+      tableName: "highlights",
+      fieldFilter: "id",
+      valueFilter: highlightId,
+    });
+  },
+  getHighlights: (db: SQLiteDatabase, bookId: string) => {
+    return dbActions.getMany({
+      db,
+      tableName: "highlights",
+      fieldFilter: "bookId",
+      valueFilter: bookId,
+    });
+  },
 };
 
 export default {
   openDatabase,
-  createBookTable,
-  dropBookTable,
-  insertBook,
-  getBooks,
-  getBook,
-  updateBookProgress,
-  createHighlightsTable,
-  dropHighlightsTable,
-  insertHighlight,
-  deleteHighlight,
-  getHighlights,
+  books,
+  highlights,
 };
